317bfaf43fdda7e7616525a7eb8023bc4845be78
datata <dani@danitarazona.dev>
2022-10-01 16:45:15 +0200

    [GREEN] fizzbuzz is a function

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
 
+const fizzbuzz = () => {
+    
+};
+
 describe('fizzbuzz', () => {
     it('should be a function', () => {
         expect(typeof fizzbuzz).toBe('function')
