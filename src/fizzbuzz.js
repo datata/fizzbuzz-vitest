1f8f1caf1a572ea5d403b21454b6ac51146682f4
datata <dani@danitarazona.dev>
2022-10-02 12:53:44 +0200

    [GREEN] return fizz if number is multiple of 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,5 +2,7 @@ export const fizzbuzz = (number) => {
     if(typeof number != 'number') throw new Error('Parameter provided is not a number.');
     if(Number.isNaN(number)) throw new Error('Parameter provided is not a number.');
 
+    if(number % 3 === 0) return 'fizz';
+
     return number;
 };
