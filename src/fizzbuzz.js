73081b580a7aafbd6825f5b5d05b872282f1d233
datata <dani@danitarazona.dev>
2022-10-02 14:33:53 +0200

    [GREEN] number is multiple 3 & 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,8 @@ export const fizzbuzz = (number) => {
     if(typeof number != 'number') throw new Error('Parameter provided is not a number.');
     if(Number.isNaN(number)) throw new Error('Parameter provided is not a number.');
 
+    if(number % 5 === 0 && number % 3 === 0) return 'fizzbuzz';
+
     if(number % 3 === 0) return 'fizz';
 
     if(number % 5 === 0) return 'buzz';
